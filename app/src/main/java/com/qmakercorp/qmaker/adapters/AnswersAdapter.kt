423a074cf63416f3836a73871c5f002d3708511c
Thomas <msamoht16@gmail.com>
2018-10-17 02:28:52 -0300

    implementa ordenação de items da lista em QuestionFragment

--- app/src/main/java/com/qmakercorp/qmaker/adapters/AnswersAdapter.kt
+++ app/src/main/java/com/qmakercorp/qmaker/adapters/AnswersAdapter.kt
@@ -1,18 +1,29 @@
 package com.qmakercorp.qmaker.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.MotionEventCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.qmakercorp.qmaker.R
+import com.qmakercorp.qmaker.components.ItemTouchHelperAdapter
+import com.qmakercorp.qmaker.components.ItemTouchHelperViewHolder
+import com.qmakercorp.qmaker.components.OnStartDragListener
 import com.qmakercorp.qmaker.data.model.Answer
 import kotlinx.android.synthetic.main.answers_item.view.*
+import java.util.*
 
 class AnswersAdapter(private val context: Context,
-                     var answers: MutableList<Answer>):
-        Adapter<AnswersAdapter.AnswersViewHolder>() {
+                     var answers: MutableList<Answer>,
+                     private val mDragStartListener: OnStartDragListener):
+        Adapter<AnswersAdapter.AnswersViewHolder>(), ItemTouchHelperAdapter {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnswersViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.answers_item, parent, false)
@@ -23,9 +34,31 @@ class AnswersAdapter(private val context: Context,
         return answers.count()
     }
 
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: AnswersViewHolder, position: Int) {
         val answer = answers[position]
         holder.bindView(answer)
+        holder.handleView.setOnTouchListener { v, event ->
+
+            if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                mDragStartListener.onStartDrag(holder)
+            }
+            false
+        }
+    }
+
+    override fun onItemDismiss(position: Int) {
+        answers.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        answers[fromPosition].id = toPosition
+        answers[toPosition].id = fromPosition
+        Collections.swap(answers, fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        return true
     }
 
     fun removeAt(position: Int) {
@@ -34,13 +67,23 @@ class AnswersAdapter(private val context: Context,
     }
 
 
-    class AnswersViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class AnswersViewHolder(itemView: View):
+            RecyclerView.ViewHolder(itemView), ItemTouchHelperViewHolder {
 
 //        val description = itemView.mainText
+        val handleView: ImageView = itemView.handle
 
         fun bindView(answer: Answer) {
 //            title.text = question.order.toString()
             itemView.mainText.text = answer.description
+        }
+
+        override fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        override fun onItemClear() {
+            itemView.setBackgroundColor(0)
         }
 
     }
--- app/src/main/java/com/qmakercorp/qmaker/ui/main/fragments/quizzes/QuestionFragment.kt
+++ app/src/main/java/com/qmakercorp/qmaker/ui/main/fragments/quizzes/QuestionFragment.kt
@@ -6,20 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.qmakercorp.qmaker.R
 import com.qmakercorp.qmaker.adapters.AnswersAdapter
+import com.qmakercorp.qmaker.components.OnStartDragListener
+import com.qmakercorp.qmaker.components.SimpleItemTouchHelperCallback
 import com.qmakercorp.qmaker.data.model.Answer
 import com.qmakercorp.qmaker.data.model.Question
 import com.qmakercorp.qmaker.data.model.Quiz
 import kotlinx.android.synthetic.main.fragment_question.*
 
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(), OnStartDragListener {
 
     private lateinit var adapter: AnswersAdapter
     private lateinit var quiz: Quiz
     private lateinit var question: Question
+    private var mItemTouchHelper: ItemTouchHelper? = null
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -35,12 +40,19 @@ class QuestionFragment : Fragment() {
         initializeViews()
     }
 
+    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder?) {
+        viewHolder?.let { mItemTouchHelper?.startDrag(it) }
+    }
+
     private fun initializeViews() {
         context?.let {
             val answers = initializeAnswers()
-            adapter = AnswersAdapter(it, answers)
+            adapter = AnswersAdapter(it, answers, this)
             rv_answers.adapter = adapter
             rv_answers.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            val callback = SimpleItemTouchHelperCallback(adapter)
+            mItemTouchHelper = ItemTouchHelper(callback)
+            mItemTouchHelper?.attachToRecyclerView(rv_answers)
         }
     }
 
